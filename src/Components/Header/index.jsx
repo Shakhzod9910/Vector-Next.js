57aa0594a33089a7cfb85b9f3fba074add41aa66
Shakhzod9910 <92552024+Shakhzod9910@users.noreply.github.com>
2022-01-20 23:28:52 +0500

    Update index.jsx

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,24 @@
 import Logo from "../../Assets/img/Logo.png"
 import Image from "next/image"
+import Menu from "../../Assets/img/menu.png"
+import Back from "../../Assets/img/back.png"
+import { useRef } from "react"
+
 function Header(){
-    
+    const active = useRef(null)
+    const x = useRef(null)
+    const y = useRef(null)
+
+    function menuActive(evt){
+        active.current.classList.add('active')
+        x.current.classList.add("orem")
+        y.current.classList.add("xact")
+    }
+    function menuClose(evt){
+        active.current.classList.remove('active')
+        x.current.classList.remove("orem")
+        y.current.classList.remove("xact")
+    }
     return(
         <header className="container">
             <div className="navbar">
@@ -9,7 +26,7 @@ function Header(){
                 <div className="img">
                 <Image className="logo" src={Logo} alt="Logo"/>
                 </div>
-                <ul>
+                <ul ref={active}>
                     <li>ABOUT</li>
                     <li>GALLERY</li>
                     <li>PRICING</li>
@@ -20,6 +37,10 @@ function Header(){
                 <div className="log_buttons">
                     <button className="sign_btn sign_in">SIGN IN</button>
                     <button className="sign_btn sign_up">SIGN UP</button>
+                    <div className="menu">
+                        <div className="open" ref={x}><Image src={Menu} onClick={menuActive}/></div>
+                        <div className="close" ref={y}><Image src={Back} onClick={menuClose}/></div>
+                    </div>
                 </div>
             </div>
         </header>
